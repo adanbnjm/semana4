47b379aee25681b1940f6aa095ae0f10ff2b70f9
adanbnjm <adanbnjm@gmail.com>
2026-08-24 22:58:51 -0400

    feat: conexión a postgres y GET /api/menu

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,35 @@
-import express, { type Request, type Response } from "express"; 
+import express, { type Request, type Response } from "express";
 import swaggerRouter from "./routes/swagger.router.js";
-import cors from "cors"
-
-const port = process.env.PORT; 
-
-
+import cors from "cors";
+import pool from "./config/db.js";
+const port = process.env.PORT;
 
 const app = express();
 
-// Middlewares 
+// Middlewares
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
-app.use("/api/docs", swaggerRouter) 
-
+app.use("/api/docs", swaggerRouter);
 
 app.get("/", (req: Request, res: Response) => {
-    /*#swagger.tags = ['Tests']*/
-    res.json({
-        status: "Server online",
-        version: "1.0.0"
-    });
+  /*#swagger.tags = ['Tests']*/
+  res.json({
+    status: "Server online",
+    version: "1.0.0",
+  });
+});
+app.get("/api/menu", async (req, res) => {
+  try {
+    console.log("DB:", process.env.DB_NAME);
+    const result = await pool.query("SELECT * FROM producto");
+    res.json(result.rows);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Error al obtener los productos" });
+  }
 });
 
 app.listen(port, () => {
-    console.log(`URL: http://localhost:${port}`);
+  console.log(`URL: http://localhost:${port}`);
 });
